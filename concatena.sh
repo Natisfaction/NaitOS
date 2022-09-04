c8d1093360269a47cd354f3efd7f9b909b60ab5a
Natisfaction <90966287+Natisfaction@users.noreply.github.com>
2022-09-04 18:02:25 +0200

    untrack files contained in the .gitignore file

--- concatena.sh
+++ concatena.sh
@@ -1,1 +1,6 @@
+nasm -f bin "src/boot.asm" -o "build/boot.bin"
+nasm -f bin "src/fine.asm" -o "build/fine.bin"
+nasm -f elf "src/start_kern.asm" -o "build/start_kern.o"
+./gcc-i686/bin/i686-elf-gcc -ffreestanding -m32 -g -c "src/kernel.cpp" -o "build/kernel.o"
+./gcc-i686/bin/i686-elf-ld -o "build/kernel.bin" -Tlinker.ld -nostdlib "C:\i686-elf-tools\lib\gcc\i686-elf\7.1.0\libgcc.a" "build/start_kern.o" "build/kernel.o"
 cat "build/boot.bin" "build/kernel.bin" "build/fine.bin" > "OS/NaitOS.img"
